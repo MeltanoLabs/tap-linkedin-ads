753ece8f92ebe3b7170b563d9cbc3c580d49ad06
ed-ruggable <edward@ryan-miranda.com>
2023-03-04 15:46:56 -0800

    updated client.py to bearer token template
    originally the SDK was set up for OAuth

--- tap_linkedin/client.py
+++ tap_linkedin/client.py
@@ -1,25 +1,24 @@
-"""REST client handling, including linkedinStream base class."""
+"""REST client handling, including LinkedInStream base class."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Callable, Iterable
 
 import requests
-from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
-
-from memoization import cached
-
+from singer_sdk.authenticators import BearerTokenAuthenticator
 from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
 
-from tap_linkedin.auth import linkedinAuthenticator
-
-
+_Auth = Callable[[requests.PreparedRequest], requests.PreparedRequest]
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
-class linkedinStream(RESTStream):
-    """linkedin stream class."""
+class LinkedInStream(RESTStream):
+    """LinkedIn stream class."""
 
     # TODO: Set the API's base URL here:
-    url_base = "https://api.linkedin.com/rest/adAnalytics"
+    url_base = "https://api.mysample.com"
 
     # OR use a dynamic url_base:
     # @property
@@ -31,23 +30,45 @@ class linkedinStream(RESTStream):
     next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
 
     @property
-    @cached
-    def authenticator(self) -> linkedinAuthenticator:
-        """Return a new authenticator object."""
-        return linkedinAuthenticator.create_for_stream(self)
+    def authenticator(self) -> BearerTokenAuthenticator:
+        """Return a new authenticator object.
+
+        Returns:
+            An authenticator instance.
+        """
+        return BearerTokenAuthenticator.create_for_stream(
+            self,
+            token=self.config.get("refresh_token", ""),
+        )
 
     @property
     def http_headers(self) -> dict:
-        """Return the http headers needed."""
+        """Return the http headers needed.
+
+        Returns:
+            A dictionary of HTTP headers.
+        """
         headers = {}
         if "user_agent" in self.config:
             headers["User-Agent"] = self.config.get("user_agent")
+        # If not using an authenticator, you may also provide inline auth headers:
+        # headers["Private-Token"] = self.config.get("refresh_token")
         return headers
 
     def get_next_page_token(
-        self, response: requests.Response, previous_token: Optional[Any]
-    ) -> Optional[Any]:
-        """Return a token for identifying next page or None if no more pages."""
+        self,
+        response: requests.Response,
+        previous_token: Any | None,
+    ) -> Any | None:
+        """Return a token for identifying next page or None if no more pages.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+            previous_token: The previous page token value.
+
+        Returns:
+            The next pagination token.
+        """
         # TODO: If pagination is required, return a token which can be used to get the
         #       next page. If this is the final page, return "None" to end the
         #       pagination loop.
@@ -63,9 +84,19 @@ class linkedinStream(RESTStream):
         return next_page_token
 
     def get_url_params(
-        self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> Dict[str, Any]:
-        """Return a dictionary of values to be used in URL parameterization."""
+        self,
+        context: dict | None,
+        next_page_token: Any | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
         params: dict = {}
         if next_page_token:
             params["page"] = next_page_token
@@ -75,21 +106,45 @@ class linkedinStream(RESTStream):
         return params
 
     def prepare_request_payload(
-        self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> Optional[dict]:
+        self,
+        context: dict | None,
+        next_page_token: Any | None,
+    ) -> dict | None:
         """Prepare the data payload for the REST API request.
 
         By default, no payload will be sent (return None).
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary with the JSON body for a POST requests.
         """
         # TODO: Delete this method if no payload is required. (Most REST APIs.)
         return None
 
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
-        """Parse the response and return an iterator of result rows."""
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
         # TODO: Parse response body and return a set of records.
         yield from extract_jsonpath(self.records_jsonpath, input=response.json())
 
-    def post_process(self, row: dict, context: Optional[dict]) -> dict:
-        """As needed, append or transform raw data to match expected structure."""
+    def post_process(self, row: dict, context: dict | None = None) -> dict | None:
+        """As needed, append or transform raw data to match expected structure.
+
+        Args:
+            row: An individual record from the stream.
+            context: The stream context.
+
+        Returns:
+            The updated record dictionary, or ``None`` to skip the record.
+        """
         # TODO: Delete this method if not needed.
         return row
