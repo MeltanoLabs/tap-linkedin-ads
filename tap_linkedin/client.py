ac0183a7167ff9f355e349524a21f1af1277beaf
ed-ruggable <edward@ryan-miranda.com>
2023-03-04 20:10:28 -0800

    update URL base for LinkedIn

--- tap_linkedin/client.py
+++ tap_linkedin/client.py
@@ -17,14 +17,7 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 class LinkedInStream(RESTStream):
     """LinkedIn stream class."""
 
-    # TODO: Set the API's base URL here:
-    url_base = "https://api.mysample.com"
-
-    # OR use a dynamic url_base:
-    # @property
-    # def url_base(self) -> str:
-    #     """Return the API URL root, configurable via tap settings."""
-    #     return self.config["api_url"]
+    url_base = "https://api.linkedin.com/"
 
     records_jsonpath = "$[*]"  # Or override `parse_response`.
     next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
