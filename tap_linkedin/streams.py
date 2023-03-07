ec357e40f49be84b7b5f032b38e5b64d1de55e3f
edward <edward@ryan-miranda.com>
2023-03-06 21:28:57 -0800

    *still need to clean* added AdAnalyticsByCampaign stream dictionary

--- tap_linkedin/streams.py
+++ tap_linkedin/streams.py
@@ -18,8 +18,8 @@ class Accounts(LinkedInStream):
     name = "accounts"
     path = "adAccounts"
     primary_keys = ["id"]
+
     replication_keys = ["last_modified_time"]
-    #schema_filepath = "tap_linkedin/schemas/accounts.json"
     schema_filepath = SCHEMAS_DIR / "accounts.json"
     tap_stream_id = "accounts"
     #replication_method = "INCREMENTAL"
@@ -30,17 +30,38 @@ class Accounts(LinkedInStream):
         "q": "search"
     }
 
+class AdAnalyticsByCampaign(LinkedInStream):
+    """
+    https://docs.microsoft.com/en-us/linkedin/marketing/integrations/ads-reporting/ads-reporting#analytics-finder
+    """
+    name = "ad_analytics_by_campaign"
+    replication_method = "INCREMENTAL"
+    schema_filepath = SCHEMAS_DIR / "ad_analytics_by_campaign.json"
+    replication_keys = ["end_at"]
+    key_properties = ["campaign_id", "start_at"]
+    account_filter = "accounts_param"
+    path = "adAnalytics"
+    foreign_key = "id"
+    data_key = "elements"
+    parent = "campaigns"
+    params = {
+        "q": "analytics",
+        "pivot": "CAMPAIGN",
+        "timeGranularity": "DAILY",
+        "count": 10000
+    }
+
 class VideoAds(LinkedInStream):
     """
     https://docs.microsoft.com/en-us/linkedin/marketing/integrations/ads/advertising-targeting/create-and-manage-video#finders
     """
     name = "video_ads"
+    path = "adDirectSponsoredContents"
     replication_keys = ["last_modified_time"]
     replication_method = "INCREMENTAL"
     key_properties = ["content_reference"]
     schema_filepath = SCHEMAS_DIR / "video_ads.json"
     foreign_key = "id"
-    path = "adDirectSponsoredContents"
     data_key = "elements"
     parent = "accounts"
     params = {
