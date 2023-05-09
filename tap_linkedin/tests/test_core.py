3e675873de1da614b1bc1cbea73e7bbfb14363bc
Dhrruv <dhrruv@Dhrruvs-MBP.bbrouter>
2023-05-09 20:16:36 +0530

    updated linkedin stream pytest

--- tap_linkedin/tests/test_core.py
+++ tap_linkedin/tests/test_core.py
@@ -6,9 +6,24 @@ from singer_sdk.testing import get_standard_tap_tests
 
 from tap_linkedin.tap import TapLinkedIn
 
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+linkedin_config = {
+    "client_secret":os.getenv("TAP_LINKEDIN_CLIENT_SECRET"),
+    "access_token":os.getenv("TAP_LINKEDIN_ACCESS_TOKEN"),
+    "refresh_token":os.getenv("TAP_LINKEDIN_REFRESH_TOKEN"),
+    "client_id":os.getenv("TAP_LINKEDIN_CLIENT_ID"),
+    "account_id":os.getenv("TAP_LINKEDIN_ACCOUNTS"),
+    "user_agent":os.getenv("TAP_LINKEDIN_USER_AGENT"),
+    "start_date":os.getenv("TAP_LINKEDIN_START_DATE"),
+    "end_date":os.getenv("TAP_LINKEDIN_END_DATE"),
+    "linkedin_version":"202207",
+    "x-restli-protocol-version":"1.0.0",
+    "owner":os.getenv("TAP_LINKEDIN_OWNER"),
+    "campaign":os.getenv("TAP_LINKEDIN_CAMPAIGN")
 }
 
 
@@ -17,7 +32,7 @@ def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
     tests = get_standard_tap_tests(
         TapLinkedIn,
-        config=SAMPLE_CONFIG
+        config=linkedin_config
     )
     for test in tests:
         test()
